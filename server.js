7ab2c940e31b285f15bb5a349ed5edfbe16f88e7
ThiagoCollante <titicoyoteweb@gmail.com>
2026-03-20 21:51:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,48 +11,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const rooms = {};
 
-// Helper to send the current active rooms to clients
 function broadcastRoomList() {
     const activeRooms = Object.keys(rooms).map(roomName => {
-        return {
-            name: roomName,
-            players: Object.keys(rooms[roomName]).length
-        };
+        return { name: roomName, players: Object.keys(rooms[roomName]).length };
     });
     io.emit('availableRooms', activeRooms);
 }
 
 io.on('connection', (socket) => {
     let currentRoom = null;
-
-    // Send the room list immediately when a user connects
     broadcastRoomList();
 
     socket.on('joinRoom', (roomName) => {
-        // Leave previous room if any (safety catch)
-        if (currentRoom) {
-            socket.leave(currentRoom);
-            if (rooms[currentRoom]) delete rooms[currentRoom][socket.id];
-        }
+        // Leave existing room if any
+        if (currentRoom) leaveCurrentRoom(socket);
 
         socket.join(roomName);
         currentRoom = roomName;
-        
         if (!rooms[currentRoom]) rooms[currentRoom] = {};
         
         rooms[currentRoom][socket.id] = {
             id: socket.id,
-            position: { x: 0, y: 1, z: 0 },
+            position: { x: 0, y: 0, z: 0 }, // Base of the player
             rotation: { y: 0 },
             color: Math.floor(Math.random() * 16777215) 
         };
 
         socket.emit('currentPlayers', rooms[currentRoom]);
         socket.to(currentRoom).emit('newPlayer', rooms[currentRoom][socket.id]);
-        
-        console.log(`[+] ${socket.id} joined ${roomName}`);
-        
-        // Update everyone's server list
         broadcastRoomList();
     });
 
@@ -64,19 +50,29 @@ io.on('connection', (socket) => {
         }
     });
 
+    // Custom leave event for the Pause Menu
+    socket.on('leaveRoom', () => {
+        leaveCurrentRoom(socket);
+        currentRoom = null;
+    });
+
     socket.on('disconnect', () => {
+        leaveCurrentRoom(socket);
+    });
+
+    // Helper function to cleanly remove a player from a room
+    function leaveCurrentRoom(sock) {
         if (currentRoom && rooms[currentRoom]) {
-            delete rooms[currentRoom][socket.id];
-            io.to(currentRoom).emit('playerDisconnected', socket.id);
+            sock.leave(currentRoom);
+            delete rooms[currentRoom][sock.id];
+            io.to(currentRoom).emit('playerDisconnected', sock.id);
             
-            // Clean up empty rooms
             if (Object.keys(rooms[currentRoom]).length === 0) {
                 delete rooms[currentRoom];
             }
-            console.log(`[-] ${socket.id} disconnected`);
             broadcastRoomList();
         }
-    });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
